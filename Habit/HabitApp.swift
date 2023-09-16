15b7e9295006262a99b49cf63a0ef162cbc11f72
Adriano Soares <eng.comp.adriano@gmail.com>
2023-09-16 15:36:14 -0400

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by user246507 on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
