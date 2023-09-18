0752563c08e8d05cc8406511cb065881b2152605
Adriano Soares <eng.comp.adriano@gmail.com>
2023-09-18 17:56:24 -0400

    Aulas 3 e 4: SplashView Loading e Error.

--- Habit/Splash/View/SplashView.swift
+++ Habit/Splash/View/SplashView.swift
@@ -8,19 +8,41 @@
 import SwiftUI
 
 struct SplashView: View {
-    @State var state: SplashUIState = .loading
+    @State var state: SplashUIState = .error("Perda de conexao com o servidor")
+    //@State var state: SplashUIState = .loading
     var body: some View {
         switch state{
         case .loading:
-            Text("Carregando")
+            loadingView()
         case .goToSignInScreen:
             Text("Tela de login")
         case .goToHomeScreen:
             Text("Tela principal")
         case .error(let msg):
-            Text("Erro:\n\(msg)")
+           loadingView(error: msg)
         }
-        
+    }
+}
+
+extension SplashView {
+    func loadingView(error: String? = nil) -> some View {
+        ZStack{
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea()
+                .background(Color.white)
+            if let error = error {
+                Text("")
+                    .alert(isPresented: .constant(true)){
+                        Alert(title: Text("Personal Habits"), message: Text(error), dismissButton: .default(Text("OK")){
+                            //acao ao fechar o botao
+                        })
+                    }
+                    .background(Color.red)
+            }
+        }
     }
 }
 
