2daf78b64e01e0843db3b90f997d6d5bab116886
Adriano Soares <eng.comp.adriano@gmail.com>
2023-12-18 09:52:54 -0300

    Download Image in HabitCardView.

--- Habit/Habit/View/HabitCardView.swift
+++ Habit/Habit/View/HabitCardView.swift
@@ -41,8 +41,17 @@ struct HabitCardView: View {
                     self.action = true
                 }, label: {
                     HStack {
-                        Image(systemName: "pencil")
-                            .foregroundColor(.black)
+                        if viewModel.icon.isEmpty {
+                            Image(systemName: "pencil")
+                                .foregroundColor(.black)
+                        }
+                        else {
+                            ImageView(url: viewModel.icon)
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 32, height: 32)
+                                .clipped()
+                        }
+                        
                         
                         HStack(alignment: .top) {
                             Spacer()
